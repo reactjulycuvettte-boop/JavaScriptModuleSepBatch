2e8cf257f7680321738ed5555458a1a102517cc0
codeWithShubhamS <codewithshubhams@gmail.com>
2025-10-12 23:16:19 +0530

    Grid and Intro to jS

---
+++ javaScript/Readme.md
@@ -0,0 +1,19 @@
+# JavaScriptModuleSepBatch
+
+// Topics to be covered:-
+
+<!-- JavaScript Module - Session - 1 - Introduction to JavaScript -->
+
+1. JS Basics.
+2. Varibales.
+3. Constants.
+4. Primitive Types.
+5. Complex Types.
+6. Objects.
+7. Arrays.
+8. Functions.
+9. Types of Funct.
+10. parseInt/parseFloat.
+11. Control Statments - Conditional - if, else, switch.
+12. Control Statments - Iteration - loops.
+13. Arrays.....
---
+++ javaScript/index.js
@@ -0,0 +1,36 @@
+// Datatypes
+
+// Primitive 
+// 1. String = 'affuyadsfyuasdgfu';
+// 2. Number = 21435;
+// 3. Boolean = true or false;
+// 4. BigInt = 3123212423432;
+// 5. Null = Intentional Nothing.
+// 6. Undefined = Declaration and Define.
+let x = null;
+
+// Compound/Complex
+
+// Array 
+
+let array = ["ssad" , 12123, true, "assdasd"]
+// Objects
+
+const object = {
+  name : "Shubham",
+  age: 29,
+  adddress: "Noida",
+  isIndian: true
+}
+
+let line = ""
+let array2 = []
+
+
+
+// Variables - Name given to the container.
+
+var firstName = "Cuvette"
+console.log(firstName)
+let age = 234;
+const wwqe = "fwqere"
